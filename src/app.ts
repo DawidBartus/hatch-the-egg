11deb3298fe7719663b5b407b1e9d9f0b1531289
Dawid <109389692+DawidBartus@users.noreply.github.com>
2023-08-07 14:28:15 +0200

    game start

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Eggs!')
-})
+import { Game } from "./modules/game.js";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const counter = document.querySelector(
+    "#counter"
+  ) as HTMLParagraphElement | null;
+  const egg = document.querySelector("#egg") as HTMLImageElement | null;
+  const result = document.querySelector(
+    "#result"
+  ) as HTMLParagraphElement | null;
+
+  const actionButton = document.querySelector(
+    "#action-btn"
+  ) as HTMLButtonElement | null;
+
+  const game = new Game();
+  game.init({
+    actionButtonElement: actionButton,
+    resultElement: result,
+    counterElement: counter,
+    eggElement: egg,
+  });
+});
